290152bb2c2ca3db12d36bfdf00d90c726614a16
KoreaMango <mgo8434@naver.com>
2021-12-24 13:11:15 +0900

    test

--- Complete_py/LRUD.py
+++ Complete_py/LRUD.py
@@ -4,8 +4,8 @@ x, y = 1, 1
 plans = input().split()
 
 # L, R, U, D 에 따른 이동
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 move_type = ['L', 'R', 'U', 'D']
 
 #이동 계획 확인
@@ -16,10 +16,10 @@ for plan in plans:
             ny = y + dy[i]
 
     # 공간을 벗어나는 경우 무시
-    if nx <1 or ny <1 or nx > n or ny > n:
+    if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
 
     # 이동 수행
     x, y = nx, ny
 
-print(x,y)
+print(x, y)
