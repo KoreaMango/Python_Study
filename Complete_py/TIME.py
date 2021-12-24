769d7ec822284d59a36d04f611267fb9addf2002
KoreaMango <mgo8434@naver.com>
2021-12-24 13:44:49 +0900

    KNIGHT

--- Complete_py/TIME.py
+++ Complete_py/TIME.py
@@ -1,16 +1,10 @@
-s = input()
-# 'UUR', 'UUL', 'LLU', 'LLD', 'DDL', 'DDR', 'RRU', 'RRD'
-# (x, y)
-move_type = [(1,-2), (-1,-2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, -1), (2, 1)]
+N = int(input())
 
-row = int(ord(s[0]) - 96)
-col = int(s[1])
 count = 0
-
-for i in range(len(move_type)):
-    nx = move_type[i][0]
-    ny = move_type[i][1]
-    if row + nx > 0 and col + ny > 0 and row + nx <= 8 and col + ny <= 8:
-        count +=1 
+for i in range(1 + N):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
 
 print(count)
