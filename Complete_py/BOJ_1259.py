ab75ede63e525a8ef8243fc257663234cf7ea718
KoreaMango <mgo8434@naver.com>
2021-12-27 19:56:49 +0900

    BOJ_1259

--- Complete_py/BOJ_1259.py
+++ Complete_py/BOJ_1259.py
@@ -4,13 +4,12 @@ while (True):
     flag = 0
     if n == "0":  # 0을 입력하였으면 종료
         break
-    else:
-        length = len(n)
-        for i in range(length):
-            if int(n[i]) - int(n[-(i+1)]) != 0:
+    else:  # 아니면 계속 실행
+        for i in range(len(n)):  # 길이 만큼 길행
+            if int(n[i]) - int(n[-(i+1)]) != 0:  # 다르면 0이 아니기 때문에 실패 플래그 설정
                 flag = 1
-                break
-    if flag == 0:
+                break  # 반복문 종료
+    if flag == 0:  # 실패 플래그가 0이면 yes
         print("yes")
-    else:
+    else:  # 실패 플래그가 1이면 no
         print("no")
