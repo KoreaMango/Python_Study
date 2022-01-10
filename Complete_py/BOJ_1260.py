2a1c872b00c4f747914f0237bbf435769d033122
KoreaMango <mgo8434@naver.com>
2022-01-10 17:27:18 +0900

    BOJ_1260, BOJ_2606

---
+++ Complete_py/BOJ_1260.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+def dfs(gragh, v, visited):
+    visited[v] = True  # 방문 노드 처리하기
+    print(v, end=' ')  # 방문한거 프린트 하기
+    for i in gragh[v]:
+        if not visited[i]:
+            dfs(gragh, i, visited)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True  # 방문 노드 처리하기
+    while queue:  # 큐가 빌 때까지
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+N, M, V = map(int, input().split())  # 입력 받기
+
+arr = [[]]  # 0번 인덱스를 제외하고 선언
+
+for i in range(M):  # 간선의 개수 M
+    arr.append(list(map(int, input().split())))  # 리스트 배열 넣기
+
+
+graph = [[]]  # 0번 인덱스를 제외하고 선언
+for i in range(N):  # 정점 개수 만큼 그래프 선언
+    graph.append(list())
+
+for i in arr[1:]:
+    graph[i[0]].append(i[1])
+    graph[i[1]].append(i[0])
+
+for i in range(N+1):
+    graph[i].sort()
+
+DFSvisited = [False] * (N + 1)  # 0번 인덱스를 제외했을 때 정점의 개수만큼 선언, 방문 노드 처리
+BFSvisited = [False] * (N + 1)
+
+
+dfs(graph, V, DFSvisited)
+print()
+bfs(graph, V, BFSvisited)
---
+++ Complete_py/BOJ_2606.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+
+def dfs(gragh, v, visited):
+    visited[v] = True  # 방문 노드 처리하기
+    for i in gragh[v]:
+        if not visited[i]:
+            dfs(gragh, i, visited)
+
+
+N = int(input())
+M = int(input())
+
+arr = [[]]  # 0번 인덱스를 제외하고 선언
+
+for i in range(M):  # 간선의 개수 M
+    arr.append(list(map(int, input().split())))  # 리스트 배열 넣기
+
+
+graph = [[]]  # 0번 인덱스를 제외하고 선언
+for i in range(N):  # 정점 개수 만큼 그래프 선언
+    graph.append(list())
+
+for i in arr[1:]:
+    graph[i[0]].append(i[1])
+    graph[i[1]].append(i[0])
+
+for i in range(N+1):
+    graph[i].sort()
+
+DFSvisited = [False] * (N + 1)  # 0번 인덱스를 제외했을 때 정점의 개수만큼 선언, 방문 노드 처리
+
+cnt = -1
+dfs(graph, 1, DFSvisited)
+
+for i in DFSvisited:
+    if i == True:
+        cnt += 1
+print(cnt)
